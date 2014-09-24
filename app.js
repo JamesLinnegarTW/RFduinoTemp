81296994d780205cbe6662dcd76c3b0d8874f0a1
root <root@raspberrypi>
2014-09-24 09:50:02 +0000

    is there a rfduino nearby

--- app.js
+++ app.js
@@ -1,49 +1,23 @@
 
-function Temperature() {
-  var led = 0;
-  var DeviceRepository = require('./DeviceRepository');
-  var ConnectionManager = require('./ConnectionManager');
+function App() {
+  var BleScanner = require('./BLEScanner');
+  var bleScan = new BleScanner();
 
-  var deviceRepo = new DeviceRepository();
-  var bluetoothConnection = new ConnectionManager(deviceRepo);
+  bleScan.start();
 
-  bluetoothConnection.start();
-
-
+  setInterval(function(){
+	console.log(bleScan.canISee('RFduino'));
+  }, 1000);
   function exitHandler(options, err) {
-    console.log('exiting test');
-    deviceRepo.toAll(function(){
-      this.disconnect();
-    });
     process.exit();
   }
 
 
-
-  function sendBuffer(){
-
-    function readCallback(){
-      return function(){
-        var device = this;
-        device.readBuffer(function(data){
-          console.log("Current Temp with " + device.getName() + " is: " + data.readFloatLE(0) + "");
-        });
-      }
-    }
-
-    var cb = readCallback();
-    deviceRepo.toAll(cb);
-
-
-  }
-
-  setInterval(sendBuffer, 5000);
-
-  process.on('exit', exitHandler.bind(null,{cleanup:true}));
-  process.on('SIGINT', exitHandler.bind(null, {exit:true}));
-  process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+//  process.on('exit', exitHandler.bind(null,{cleanup:true}));
+ // process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+ // process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
 
 
 }
 
-Temperature();
+App();
