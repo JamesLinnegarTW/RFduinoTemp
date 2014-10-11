c3d427e16c4192926d6798dd7254b6f467ae0ed5
James Linnegar <jlinnega@thoughtworks.com>
2014-10-11 18:15:48 +1100

    Revert "is there a rfduino nearby"
    This reverts commit 81296994d780205cbe6662dcd76c3b0d8874f0a1.

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
 
-function App() {
-  var BleScanner = require('./BLEScanner');
-  var bleScan = new BleScanner();
+function Temperature() {
+  var led = 0;
+  var DeviceRepository = require('./DeviceRepository');
+  var ConnectionManager = require('./ConnectionManager');
 
-  bleScan.start();
+  var deviceRepo = new DeviceRepository();
+  var bluetoothConnection = new ConnectionManager(deviceRepo);
 
-  setInterval(function(){
-	console.log(bleScan.canISee('RFduino'));
-  }, 1000);
+  bluetoothConnection.start();
+
+
   function exitHandler(options, err) {
+    console.log('exiting test');
+    deviceRepo.toAll(function(){
+      this.disconnect();
+    });
     process.exit();
   }
 
 
-//  process.on('exit', exitHandler.bind(null,{cleanup:true}));
- // process.on('SIGINT', exitHandler.bind(null, {exit:true}));
- // process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+
+  function sendBuffer(){
+
+    function readCallback(){
+      return function(){
+        var device = this;
+        device.readBuffer(function(data){
+          console.log("Current Temp with " + device.getName() + " is: " + data.readFloatLE(0) + "");
+        });
+      }
+    }
+
+    var cb = readCallback();
+    deviceRepo.toAll(cb);
+
+
+  }
+
+  setInterval(sendBuffer, 5000);
+
+  process.on('exit', exitHandler.bind(null,{cleanup:true}));
+  process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+  process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
 
 
 }
 
-App();
+Temperature();
